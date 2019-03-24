a8e294236b00a9ea88c0f0a3273357301a32e1fb
Mike Santoro <mike.santoro42@gmail.com>
2019-03-24 13:50:09 -0400

    started mar24 solution

---
+++ 03-24-2019/solution.js
@@ -0,0 +1,6 @@
+// 03-24-2019
+// Daily Coding Problem: Problem #121 [Hard]
+
+// Given a string which we can delete at most k, return whether you can make a palindrome.
+
+// For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
