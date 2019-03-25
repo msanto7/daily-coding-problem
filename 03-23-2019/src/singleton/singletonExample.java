bb492d6778f9a2da9d11697b62aa6806a332ba29
Mike Santoro <mike.santoro42@gmail.com>
2019-03-25 18:12:51 -0400

    singelton storing 2 instances

--- 03-23-2019/src/singleton/singletonExample.java
+++ 03-23-2019/src/singleton/singletonExample.java
@@ -3,15 +3,17 @@ package singleton;
 public class singletonExample {
 	
 	private static singletonExample firstInstance = null;
+	private static singletonExample secondInstance = null;
 	
 	private singletonExample() { }
 	
 	public static singletonExample getInstance() {
 		
 		// lazy init -- only created if necessary
-		if (firstInstance == null) {
+		if (firstInstance == null && secondInstance == null) {
 			
 			firstInstance = new singletonExample();
+			secondInstance = new singletonExample();
 			
 		}
 		
